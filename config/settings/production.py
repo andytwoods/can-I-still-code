c735a01da9c17d52b46dcd7ae32cde97b4b6884c
andytwoods <andytwoods@gmail.com>
2026-03-28 13:31:19 +0000

    Set default values for email credentials in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -80,8 +80,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
     default="Can I Still Code <noreply@canistillcode.org>",
