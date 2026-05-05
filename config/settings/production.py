97cf8a1382a541e7b113254a44bc614de18a374a
Andy Woods <andytwoods@users.noreply.github.com>
2026-05-05 10:27:22 +0100

    ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
-    default=["canistillcode.org", "www.canistillcode.org", "127.0.0.1", "localhost"],
+    default=["canistillcode.org", "www.canistillcode.org", "127.0.0.1", "localhost", 'agenticbrainrot.applikuapp.com',],
 )
 
 
